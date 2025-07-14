06ff7f54c7fbf652089bcb58a7f4c727d563a97f
unknown <kushaanmahajan1718@gmail.com>
2025-07-14 16:35:19 +0530

    logs

--- backend/index.js
+++ backend/index.js
@@ -52,6 +52,7 @@ app.use(cors({
 app.options('*', cors()); // Allow preflight requests
 
 app.use((req, res, next) => {
+  console.log("Incoming Origin:", req.headers.origin);
   res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
