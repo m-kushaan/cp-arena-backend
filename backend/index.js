1b83a7e9c2efb22da02d7ff2ac0dadf18a937ab5
unknown <kushaanmahajan1718@gmail.com>
2025-07-14 16:29:35 +0530

    reviews

--- backend/index.js
+++ backend/index.js
@@ -51,6 +51,18 @@ app.use(cors({
 // Must be placed before all route handlers
 app.options('*', cors()); // Allow preflight requests
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin || "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+
 // Middleware
 app.use(express.json());
 
